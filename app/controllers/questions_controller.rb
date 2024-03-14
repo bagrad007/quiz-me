e2399df70c5991e9ef0e70ff9ba46bf427a42e32
bagrad007 <bagrad007@gmail.com>
2024-03-14 15:08:40 -0400

    reverts change to root route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,10 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[ show edit update destroy ]
 
-
+  # GET /questions or /questions.json
+  def index
+    @questions = Question.all
+  end
 
   # GET /questions/1 or /questions/1.json
   def show
@@ -12,7 +15,9 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
-
+  # GET /questions/1/edit
+  def edit
+  end
 
   # POST /questions or /questions.json
   def create
@@ -29,7 +34,28 @@ class QuestionsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /questions/1 or /questions/1.json
+  def update
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to question_url(@question), notice: "Question was successfully updated." }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  # DELETE /questions/1 or /questions/1.json
+  def destroy
+    @question.destroy
+
+    respond_to do |format|
+      format.html { redirect_to questions_url, notice: "Question was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
